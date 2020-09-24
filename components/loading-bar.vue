ed4097f418ec062a4d5bfac7f86c5f91e679e48c
Eom Hongjae <ox1351@naver.com>
2020-09-24 21:09:58 +0900

    Change loadingbar color

--- components/loading-bar.vue
+++ components/loading-bar.vue
@@ -1,32 +1,38 @@
 <template>
-  <v-card>
-    <v-footer absolute class="font-weight-medium">
-      <v-col class="text-center" cols="12">
-        <button id="playButton" onclick="buttonClicked()">Pause</button>
-        <input
-          id="slider"
-          type="range"
-          min="0"
-          max="100"
-          value="0"
-          oninput="onSliderDrag(this.value)"
-        />
-      </v-col>
-    </v-footer>
-  </v-card>
+  <v-footer absolute color="#292c31" class="font-weight-medium">
+    <v-col class="text-center" cols="12">
+      <button id="playButton" onclick="buttonClicked()">Pause</button>
+      <input
+        id="slider"
+        type="range"
+        min="0"
+        max="100"
+        value="0"
+        oninput="onSliderDrag(this.value)"
+      />
+    </v-col>
+  </v-footer>
 </template>
 
 <script>
 module.exports = {
   name: "loading-bar",
   // props: ["user"],
-  // data: function () {
-  //   return {
-  //     who: "world",      
-  //   };
-  // },
+  data() {
+    return {};
+  },
 };
 </script>
 
-<style>
+<style scoped>
+button {
+  border-radius: 2px;
+  background-color: #9e9e9e; 
+  color: black;
+  padding: 10px 20px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+}
 </style>
