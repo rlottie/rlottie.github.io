cbe7f74b1813f412a24608161fd4d7ed0084563a
Eom Hongjae <ox1351@naver.com>
2020-09-24 19:54:22 +0900

    Footer 틀잡기

--- components/loading-bar.vue
+++ components/loading-bar.vue
@@ -1,11 +1,30 @@
 <template>
-    <div>loading-bar</div>
+  <v-card height="150">
+    <v-footer absolute class="font-weight-medium">
+      <v-col class="text-center" cols="12">
+        <button id="playButton" onclick="buttonClicked()">Pause</button>
+        <input
+          id="slider"
+          type="range"
+          min="0"
+          max="100"
+          value="0"
+          oninput="onSliderDrag(this.value)"
+        />
+      </v-col>
+    </v-footer>
+  </v-card>
 </template>
 
 <script>
 module.exports = {
-
-}
+  // props: ["user"],
+  // data: function () {
+  //   return {
+  //     who: "world",      
+  //   };
+  // },
+};
 </script>
 
 <style>
