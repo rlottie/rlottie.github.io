6b96000035a997b776b61cb41bda8ae82a3bd66a
ehj0128 <56681346+ehj0128@users.noreply.github.com>
2020-09-25 01:59:38 +0900

    Update index.html (#2)
    * [Fix] index->origin
    * component 기본구조
    * Delete test Files
    * Fix File name
    * left bar and color
    * Footer 틀잡기
    * background color
    * Feat/Front - Change Background Color with color Picker
    * Change loadingbar color
    * change state of Detail bar when click MenuBTN
    Co-authored-by: SangEun <eight34@naver.com>

---
+++ components/loading-bar.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-footer absolute color="#292c31" class="font-weight-medium" style="margin-=">
+    <v-col class="text-center" cols="12">
+      <button id="playButton" onclick="buttonClicked()">Pause</button>
+      <input
+        id="slider"
+        type="range"
+        min="0"
+        max="100"
+        value="0"
+        oninput="onSliderDrag(this.value)"
+      />
+    </v-col>
+  </v-footer>
+</template>
+
+<script>
+module.exports = {
+  name: "loading-bar",
+  // props: ["user"],
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style scoped>
+button {
+  border-radius: 2px;
+  background-color: #9e9e9e; 
+  color: black;
+  padding: 10px 20px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+}
+</style>
---
+++ components/right-bar.vue
@@ -0,0 +1,12 @@
+<template>
+    <div></div>
+</template>
+
+<script>
+module.exports = {
+    name:"right-bar",
+}
+</script>
+
+<style>
+</style>
