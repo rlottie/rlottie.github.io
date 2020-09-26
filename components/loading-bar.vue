527715f82e09398dcb80c6ce551870ed5105cd38
msnodeve <msnodeve@gmail.com>
2020-09-26 17:18:28 +0900

    Apply preview ui progress bar to vue.js

--- components/loading-bar.vue
+++ components/loading-bar.vue
@@ -1,16 +1,25 @@
 <template>
   <v-footer absolute color="#292c31" class="font-weight-medium" style="margin-=">
-    <v-col class="text-center" cols="12">
-      <button id="playButton" onclick="buttonClicked()">Pause</button>
-      <input
-        id="slider"
-        type="range"
-        min="0"
-        max="100"
-        value="0"
-        oninput="onSliderDrag(this.value)"
-      />
-    </v-col>
+    <v-row>
+      <v-col cols="4">
+        <button id="playButton" onclick="buttonClicked()">Pause</button>
+      </v-col>
+      <v-col cols="4" class="text-center">
+        <v-slider
+          class="v-slider--active v-slider--focused"
+          v-model="curFrame"
+          min="0"
+          ref="slider"
+          :max="allFrame"
+          :thumb-size="24"
+          thumb-label="always"
+          @Click="gotoFrame"
+        ></v-slider>
+      </v-col>
+      <v-col cols="4">
+        test
+      </v-col>
+    </v-row>
   </v-footer>
 </template>
 
@@ -19,7 +28,31 @@ module.exports = {
   name: "loading-bar",
   // props: ["user"],
   data() {
-    return {};
+    return {
+      allFrame: 137,
+      curFrame: 0,
+    };
+  },
+  mounted() {
+    var setFrame = this.setFrame;
+    var setCurFrame = this.setCurFrame;
+    window.addEventListener("AllFrameEvent", function(e){
+      setFrame(e.detail.frame);
+    })
+    window.addEventListener("CurrentFrameEvent", function(e){
+      setCurFrame(e.detail.frame);
+    })
+  },
+  methods: {
+    setFrame(value){
+      this.allFrame = value;
+    },
+    setCurFrame(value){
+      this.curFrame = value;
+    },
+    gotoFrame(value){
+      onSliderDrag(this.curFrame);
+    }
   },
 };
 </script>
@@ -34,5 +67,9 @@ button {
   text-decoration: none;
   display: inline-block;
   font-size: 16px;
+}
+
+.v-slider__thumb-container, .v-slider__track-background, .v-slider__track-fill {
+  transition: none;
 }
 </style>
--- module-rlottie.js
+++ module-rlottie.js
@@ -1,204 +1,210 @@
 function setup() {
-    var head = document.head;
-    var script = document.createElement('script');
-    script.type = 'text/javascript';
-    script.src = 'rlottie-wasm.js';
-    head.appendChild(script);
+  var head = document.head;
+  var script = document.createElement('script');
+  script.type = 'text/javascript';
+  script.src = 'rlottie-wasm.js';
+  head.appendChild(script);
 
-    script.onload = _ => {
-      Module.onRuntimeInitialized = _ => {
-        RLottieModule.init();
-      };
+  script.onload = _ => {
+    Module.onRuntimeInitialized = _ => {
+      RLottieModule.init();
     };
+  };
 }
 
 setup();
 
 // Create a LottieView Module responsible of rendering a lotti file
 var RLottieModule = (function () {
-    // create a object;
-    var obj = {};
+  // create a object;
+  var obj = {};
 
-    obj.canvas = {};
-    obj.context = {};
-    obj.lottieHandle = 0;
-    obj.frameCount = 0;
-    obj.curFrame = 0;
-    obj.frameRate = 0;
-    obj.rafId = {};
-    obj.resizeId = {};
-    obj.playing = true;
-    obj.wasPlaying = false;
+  obj.canvas = {};
+  obj.context = {};
+  obj.lottieHandle = 0;
+  obj.frameCount = 0;
+  obj.curFrame = 0;
+  obj.frameRate = 0;
+  obj.rafId = {};
+  obj.resizeId = {};
+  obj.playing = true;
+  obj.wasPlaying = false;
 
-    obj.init = function () {
-        var input = document.getElementById('fileSelector');
-        input.addEventListener('change', fileSelectionChanged);
-        window.addEventListener('dragover', handleDragOver, false);
-        window.addEventListener('drop', handleFileSelect, false);
-        window.addEventListener('resize',windowResize);
-        relayoutCanvas();
-        obj.canvas = document.getElementById("myCanvas");
-        obj.context = obj.canvas.getContext('2d');
+  obj.init = function () {
+      var input = document.getElementById('fileSelector');
+      input.addEventListener('change', fileSelectionChanged);
+      window.addEventListener('dragover', handleDragOver, false);
+      window.addEventListener('drop', handleFileSelect, false);
+      window.addEventListener('resize',windowResize);
+      relayoutCanvas();
+      obj.canvas = document.getElementById("myCanvas");
+      obj.context = obj.canvas.getContext('2d');
 
-        obj.lottieHandle = new Module.RlottieWasm();
-        obj.frameCount = obj.lottieHandle.frames();
-        // hook to the main loop
-        mainLoop();
-    }
-
-    obj.render = function () {
-        if (obj.canvas.width == 0  || obj.canvas.height == 0) return;
-
-        var buffer = obj.lottieHandle.render(obj.curFrame++, obj.canvas.width, obj.canvas.height);
-        var result = Uint8ClampedArray.from(buffer);
-        var imageData = new ImageData(result, obj.canvas.width, obj.canvas.height);
-
-        obj.context.putImageData(imageData, 0, 0);
-
-        if (obj.curFrame >=  obj.frameCount) obj.curFrame = 0;
-    }
-
-    obj.reload = function (jsString) {
-      var len  = obj.lottieHandle.load(jsString);
+      obj.lottieHandle = new Module.RlottieWasm();
       obj.frameCount = obj.lottieHandle.frames();
-      obj.curFrame = 0;
-      // force a render in pause state
-      sliderReset();
-      obj.update();
-    }
+      // hook to the main loop
+      mainLoop();
+  }
 
-    obj.update = function () {
-      if (!obj.playing)
-        window.requestAnimationFrame( obj.render);
-    }
+  obj.render = function () {
+    if (obj.canvas.width == 0  || obj.canvas.height == 0) return;
+    obj.curFrame = obj.curFrame + obj.frameRate + 1;
+    if (obj.curFrame > obj.frameCount) obj.curFrame = 0;
+    if (obj.curFrame < 0) obj.curFrame = obj.frameCount;
+    var buffer = obj.lottieHandle.render(obj.curFrame, obj.canvas.width, obj.canvas.height);
+    var result = Uint8ClampedArray.from(buffer);
+    var imageData = new ImageData(result, obj.canvas.width, obj.canvas.height);
+    obj.context.putImageData(imageData, 0, 0);
+    
+    var getCurFrameEvent = new CustomEvent("CurrentFrameEvent", {
+      detail:{
+        frame: obj.curFrame
+      }
+    });
+    window.dispatchEvent(getCurFrameEvent);
 
-     obj.pause = function () {
-        window.cancelAnimationFrame( obj.rafId);
-        obj.playing = false;
-     }
+    var getAllFrameEvent = new CustomEvent("AllFrameEvent", {
+      detail:{
+        frame: obj.frameCount
+      }
+    });
+    window.dispatchEvent(getAllFrameEvent);
+  }
 
-     obj.play = function () {
-        obj.playing = true;
-        mainLoop();
-     }
-     obj.isPlaying = function ()  {
-         return obj.playing;
-     }
+  obj.reload = function (jsString) {
+    var len  = obj.lottieHandle.load(jsString);
+    obj.frameCount = obj.lottieHandle.frames();
+    obj.curFrame = obj.frameRate > 0 ? 0 : obj.frameCount;
 
-     obj.seek = function (value) {
-        obj.curFrame = value;
-        window.requestAnimationFrame( obj.render);
-     }
+    // force a render in pause state
+    obj.update();
+  }
 
-     function mainLoop() {
-        obj.rafId = window.requestAnimationFrame( mainLoop );
-        obj.render();
-        document.getElementById("slider").max = obj.frameCount;
-        document.getElementById("slider").value = obj.curFrame;
-     }
+  obj.update = function () {
+    if (!obj.playing)
+      window.requestAnimationFrame( obj.render);
+  }
 
-    function relayoutCanvas() {
-      var width = document.getElementById("content").clientWidth;
-      var height = document.getElementById("content").clientHeight;
-      var size = width;
-      if (width < height)
-        size = width;
-      else
-        size = height;
-      size = size-8;
+   obj.pause = function () {
+      window.cancelAnimationFrame( obj.rafId);
+      obj.playing = false;
+   }
 
-      document.getElementById("myCanvas").width  = size;
-      document.getElementById("myCanvas").height  = size;
-    }
+   obj.play = function () {
+      obj.playing = true;
+      mainLoop();
+   }
+   obj.isPlaying = function ()  {
+       return obj.playing;
+   }
 
-     function windowResizeDone() {
-        relayoutCanvas();
-        if (obj.wasPlaying) {
-          obj.wasPlaying = false;
-          obj.play();
-        } else {
-          obj.update();
+   obj.seek = function (value) {
+      obj.curFrame = value;
+      window.requestAnimationFrame(obj.render);
+   }
+
+   function mainLoop() {
+      obj.rafId = window.requestAnimationFrame( mainLoop );
+      obj.render();
+   }
+
+  function relayoutCanvas() {
+    var width = document.getElementById("content").clientWidth;
+    var height = document.getElementById("content").clientHeight;
+    var size = width;
+    if (width < height)
+      size = width;
+    else
+      size = height;
+    size = size-8;
+
+    document.getElementById("myCanvas").width  = size;
+    document.getElementById("myCanvas").height  = size;
+  }
+
+   function windowResizeDone() {
+      relayoutCanvas();
+      if (obj.wasPlaying) {
+        obj.wasPlaying = false;
+        obj.play();
+      } else {
+        obj.update();
+      }
+   }
+
+   function windowResize() {
+        if (obj.isPlaying()) {
+          obj.wasPlaying = true;
+          obj.pause();
         }
-     }
+        clearTimeout(obj.resizeId);
+        obj.resizeId = setTimeout(windowResizeDone, 150);
+   }
 
-     function windowResize() {
-          if (obj.isPlaying()) {
-            obj.wasPlaying = true;
-            obj.pause();
-          }
-          clearTimeout(obj.resizeId);
-          obj.resizeId = setTimeout(windowResizeDone, 150);
-     }
-
-    return obj;
+  return obj;
 }());
 
 
 function buttonClicked() {
-    if (RLottieModule.isPlaying()) {
-        document.getElementById("playButton").innerText = "Play";
-        RLottieModule.pause();
-    } else {
-        document.getElementById("playButton").innerText = "Pause";
-        RLottieModule.play();
-    }
+  if (RLottieModule.isPlaying()) {
+      document.getElementById("playButton").innerText = "Play";
+      RLottieModule.pause();
+  } else {
+      document.getElementById("playButton").innerText = "Pause";
+      RLottieModule.play();
+  }
 }
 
 function onSliderDrag(value) {
-    RLottieModule.seek(value);
-}
-
-function sliderReset() {
-    document.getElementById("slider").max = RLottieModule.frameCount;
-    document.getElementById("slider").value = RLottieModule.curFrame;
+  RLottieModule.seek(value);
 }
 
 function handleFileSelect(evt) {
-    evt.stopPropagation();
-    evt.preventDefault();
+  evt.stopPropagation();
+  evt.preventDefault();
 
-    handleFiles(evt.dataTransfer.files);
+  handleFiles(evt.dataTransfer.files);
 }
 
 function handleFiles(files) {
-    for (var i = 0, f; f = files[i]; i++) {
-      if (f.type.includes('json')) {
-        var read = new FileReader();
-        read.readAsText(f);
-        read.onloadend = function(){
-            RLottieModule.reload(read.result);
-        }
-        break;
+  for (var i = 0, f; f = files[i]; i++) {
+    if (f.type.includes('json')) {
+      var read = new FileReader();
+      read.readAsText(f);
+      read.onloadend = function(){
+          RLottieModule.reload(read.result);
       }
+      break;
     }
+  }
 }
 
 function handleDragOver(evt) {
-  evt.stopPropagation();
-  evt.preventDefault();
-  evt.dataTransfer.dropEffect = 'copy';
+evt.stopPropagation();
+evt.preventDefault();
+evt.dataTransfer.dropEffect = 'copy';
 }
 
 function fileSelectionChanged() {
-    var input = document.getElementById('fileSelector');
-    handleFiles(input.files);
+  var input = document.getElementById('fileSelector');
+  handleFiles(input.files);
 }
 
 function onResizeSliderDrag(value) {
-  var width = document.getElementById("content").clientWidth;
-  var height = document.getElementById("content").clientHeight;
-  var size = width;
-  if (width < height)
-    size = width;
-  else
-    size = height;
-  size = size-8;
-  size = size * (value / 100);
+var width = document.getElementById("content").clientWidth;
+var height = document.getElementById("content").clientHeight;
+var size = width;
+if (width < height)
+  size = width;
+else
+  size = height;
+size = size-8;
+size = size * (value / 100);
 
-  if (size < 10 )
-    size = 10;
-  size = size | 0;
-  document.getElementById("myCanvas").width  = size;
-  document.getElementById("myCanvas").height  = size;
-  RLottieModule.update();
+if (size < 10 )
+  size = 10;
+size = size | 0;
+document.getElementById("myCanvas").width  = size;
+document.getElementById("myCanvas").height  = size;
+RLottieModule.update();
 }
