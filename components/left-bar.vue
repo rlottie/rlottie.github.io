d404e660c517a9af50eb7567371c1b0b056a7677
SangEun <eight34@naver.com>
2020-09-27 15:28:29 +0900

    check

--- components/left-bar.vue
+++ components/left-bar.vue
@@ -11,7 +11,7 @@
           </v-btn>
         </template> 
         <span>
-          upload New Json File
+          upload New JSON File
         </span>
       </v-tooltip>
     </div>
@@ -158,11 +158,11 @@ module.exports = {
   },
   methods:{
     clickMenu(i){
+      console.log(this.selectedmenu)
       this.$emit('menuclicked', i)
     },
     isActivate: function(i) {
       if (this.selectedmenu === i){
-        console.log('hi');
         return true
       }else{
         return false
