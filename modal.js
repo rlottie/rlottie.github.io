d16b607bfeeae37af1c3b12e1e4c55da75100c85
김시영 <ssks1013@gmail.com>
2020-10-02 20:15:02 +0900

    API

--- modal.js
+++ modal.js
@@ -1,6 +1,6 @@
 let modal = document.querySelector(".modal-bg");
 let closeBtn = document.querySelector("#modalCloseBtn");
-let openBtn = document.querySelector("#modalOpenBtn");
+let openBtn = document.querySelector("#getByUrl");
 
 closeBtn.addEventListener('click', ()=>{
     modal.style.display = "none";
